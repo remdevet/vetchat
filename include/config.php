c75233d3206431f130f3c6af9985efff92462922
remember mabunda <remember.mabunda@gmail.com>
2016-06-16 18:13:35 +0200

    Added vetchat

---
+++ include/config.php
@@ -0,0 +1,17 @@
+
+<?php
+/**
+ * Database configuration
+ */
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'remy');
+define('DB_HOST', '104.236.196.7');
+define('DB_NAME', 'gcm_chat');
+ 
+define("GOOGLE_API_KEY", "AIzaSyDYuU1VqiHZ5tbrVQ2bNBgozKI_W3Qy04Y");
+ 
+// push notification flags
+define('PUSH_FLAG_CHATROOM', 1);
+define('PUSH_FLAG_USER', 2);
+ 
+?
